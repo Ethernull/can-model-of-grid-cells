50cb1880dccb21f7ebbff09be8bfda953c1d0b5c
Mehmet Atak <mehmet.atak@rub.de>
2019-12-05 13:08:54 +0100

    Replace test.py

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 from can import CAN
 
 
-can = CAN(size=20, tau=8, dt=0.1, kappa=4, beta=-8,movement_mode=0)
+can = CAN(size=20, tau=8, dt=0.1, kappa=4, beta=-8, h=0,movement_mode=0,von_mises=False)
 can.run(sim_time=100)
 can.plot_activities()
