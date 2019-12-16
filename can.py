bc3f719adc2bc2e648177222d5021820e933db13
Mehmet Atak <mehmet.atak@rub.de>
2019-12-16 11:09:16 +0100

    Replace can.py

--- can.py
+++ can.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 
 class CAN:
     def __init__(self, size, tau, dt, kappa, beta, h,movement_mode, plot_weights=True, von_mises=True):
+        self.size = size
         self.u = np.random.random(size) 
         self.u_log = []
         self.tau = tau
@@ -13,15 +14,21 @@ class CAN:
         self.von_mises = von_mises
         self.mode = movement_mode #0: none #1: right-only #2: left-only #3: random
 
-        #TODO replace shifting layers
-        self.u_shift = []
-        self.u_shift.append(self.u.copy())  #still layer
-        self.u_shift.append(self.u.copy())  #right layer (placeholder)
-        self.u_shift.append(self.u.copy())  #left  layer (placeholder)
+        self.movement = np.full((size,),self.movement_signal()[0])
+        #self.u_shift = (self.u - np.ones(size) + self.movement).clip(0)
+
+        self.movement_r = np.full((size,),self.movement_signal()[1])
+        #self.u_shift_r = (self.u - np.ones(size) + self.movement_r).clip(0)
+        
+        self.movement_l = np.full((size,),self.movement_signal()[2])
+        #self.u_shift_l = (self.u - np.ones(size) + self.movement_l).clip(0)
 
         #Create weight matrix
         self.w = np.empty((size, size))
+        self.w_right = np.empty((size,size))
+        self.w_left = np.empty((size,size))
         phases = np.arange(-np.pi, np.pi, 2*np.pi/size)
+        weight_shift = np.pi/10
 
         #Connectivity values used in for weight matrix, Sanarmirskaya and Schöner - 2010 
         sigma = 1.0 #range
@@ -32,13 +39,13 @@ class CAN:
         if von_mises:
             for input_num, input_phase in enumerate(phases):
                 self.w[:, input_num] = np.exp(kappa * np.cos(phases - input_phase))/np.exp(kappa)
+                self.w_right[:, input_num] = np.exp(kappa * np.cos(phases + weight_shift - input_phase))/np.exp(kappa)
+                self.w_left[:, input_num] = np.exp(kappa * np.cos(phases - weight_shift - input_phase))/np.exp(kappa)
         else:   
             for input_num, input_phase in enumerate(phases):
-                self.w[:,input_num] = c_exc * np.exp( - pow((phases-input_phase),2) / (2* pow(sigma,2))) - c_inh
-
-        #Create multiple weight matrices based on needed shifting directions
-        self.w_right = np.roll(self.w.copy(),10,axis=1)
-        self.w_left = np.roll(self.w.copy(),10,axis=1)
+                self.w[:, input_num] = c_exc * np.exp( - pow((phases-input_phase),2) / (2* pow(sigma, 2))) - c_inh
+                self.w_right[:, input_num] = c_exc * np.exp( - pow((phases + weight_shift - input_phase), 2) / (2 * pow(sigma, 2))) - c_inh
+                self.w_let[:, input_num] = c_exc * np.exp( - pow((phases - weight_shift - input_phase), 2) / (2 * pow(sigma, 2))) - c_inh
         
         if plot_weights:
             fig, ax = plt.subplots()
@@ -52,18 +59,12 @@ class CAN:
 
     #---Consideration: Create new object class---
     def no_movement(self):
-        dir_weights = []
-        dir_weights.append(1.0)
-        dir_weights.append(0.0)
-        dir_weights.append(0.0)
-        return dir_weights
+        directional_bias = [1.0,0,0]
+        return directional_bias
 
     def moving_right(self):
-        dir_weights = []
-        dir_weights.append(0.0)
-        dir_weights.append(1.0)
-        dir_weights.append(0.0)
-        return dir_weights
+        directional_bias = [0,1.0,0]
+        return directional_bias
     
     #def moving_right(self):
     #    dir_weights = []
@@ -85,31 +86,31 @@ class CAN:
     #------
     
     def run(self, sim_time):
-        move_signal = self.movement_signal()
-        #threshold = 0.5
-        
         for step_num in range(int(round(sim_time/self.dt)) + 1):
+            u_out = 1/(1 + np.exp(self.beta*(self.u - 0.5)))
+            #Update all shifting layers: f(g-1+m)
+            self.u_shift = (u_out - np.ones(self.size) + self.movement).clip(0)
+            self.u_shift_r = (u_out - np.ones(self.size) + self.movement_r).clip(0)
+            self.u_shift_l = (u_out - np.ones(self.size) + self.movement_l).clip(0)
+            #print(self.u_shift_r)
             
-            u_out = 1/(1 + np.exp(self.beta*(self.u - 0.5)))    #Activation function [Beta = -8](? = 0.5)
-            exc_input = np.dot(u_out, self.w)                   #Sum: f(u(x, t)ω(x − x')dx'
-
-            #TODO use shifting layers based on movement_signal and gridcell input
-            #if move_signal[1] * self_u > threshold
-            #u_out_right = 1/(1 + np.exp(self.beta*(self.u_shift[1] - 0.5)))
-            #exc_input = np.dot(u_out_right, self.w_right)
+            #exc_input = np.dot(u_out, self.w) #Old method
+            #exc_input = np.dot(u_out, self.w_right) #Testing right shift
+            
+            exc_input = np.dot(self.u_shift, self.w) #New method using shifting layer
+            exc_input_r = np.dot(self.u_shift_r,self.w_right)
+            exc_input_l = np.dot(self.u_shift_r,self.w_left)
             
             if self.von_mises:
                 inh_input = max(0, np.sum(u_out) - 1)               #Inhibition, needed for von Mises distribution
             else:
                 inh_input = 0
-            
+
             self.u += (-self.u + exc_input - inh_input - self.h)/self.tau*self.dt
+            self.u += exc_input_r
             self.u_log.append(self.u.copy())
             
-            #Update all shifting layers
-            self.u_shift[0] = self.u.copy()
-            #self.u_shift[1] = self.u
-            #self.u_shift[2] = self.u
+            
 
     def plot_activities(self):
         fig, ax = plt.subplots()
