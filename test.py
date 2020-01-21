8255f4c38642b65bacc450d917c15edec0cc83cd
Mehmet Atak <mehmet.atak@rub.de>
2020-01-21 16:30:34 +0100

    Update test.py

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 from can import CAN
 
-
-can = CAN(size=20, tau=8, dt=0.1, kappa=4, beta=-8, h=0,movement_mode=0,von_mises=True)
+#Initially: tau = 8, kappa = 4
+can = CAN(size=20, tau=2, dt=0.1, kappa=0.5, beta=-8, h=0,movement_mode=0,von_mises=True)
 can.run(sim_time=200)
 can.plot_activities()
